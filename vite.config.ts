0fc435cabf94caa57853884bb24d2e6e0d98345b
Akif <akifshah7@gmail.com>
2025-04-14 19:31:59 +0530

    absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Map @ to your src directory
+    },
+  },
   plugins: [react(), tailwindcss()],
-})
+  server:{
+    port: 8686,
+  }
+});
