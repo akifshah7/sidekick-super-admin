215fa50baf506240693211093625fda2587a2f83
Akif <akifshah7@gmail.com>
2025-04-14 15:23:47 +0530

    tailwind + apollo setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ApolloProvider } from '@apollo/client'
+import client from './api/client.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 )
